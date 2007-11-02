5983456c7ef2bc73c5e07548473a58a30c5deb98
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-11-02 08:36:45 +0000

    Improved the test case for sparse_grid_2::entry_count, and added one for tile_count.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@368 add3d541-d23a-0410-8454-c3306b179d20

---
+++ src/cpp/siblings/test/sparse_grid_2_test.cpp
@@ -0,0 +1,44 @@
+#include "../sparse_grid_2.hpp"
+#include <cassert>
+
+using namespace siblings;
+
+namespace {
+    void test_entry_count()
+    {
+        sparse_grid_2 g;
+        assert(g.entry_count() == 0);
+        for (int i = 1; i <= 10; ++i) {
+            g.insert(i, circle_2(i, i, i));
+        }
+        assert(g.entry_count() == 10);
+        for (int i = 1; i <= 5; ++i) {
+            g.erase(i);
+        }
+        assert(g.entry_count() == 5);
+        for (int i = 6; i <= 10; ++i) {
+            g.erase(i);
+        }
+        assert(g.entry_count() == 0);
+    }
+
+    void test_tile_count()
+    {
+        sparse_grid_2 g;
+        assert(g.tile_count() == 0);
+        g.insert(1, circle_2(0.5, 0.5, 0.25));
+        assert(g.tile_count() == 1);
+        g.insert(2, circle_2(10, 10, 0.5));
+        assert(g.tile_count() == 5);
+        g.erase(1);
+        assert(g.tile_count() == 4);
+        g.erase(2);
+        assert(g.tile_count() == 0);
+    }
+}
+
+int main()
+{
+    test_entry_count();
+    return 0;
+}
