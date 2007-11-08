51cdcd2ea054ee9c972006f3dca662e4a11116d6
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-11-08 11:03:54 +0000

    Refactored unordered_map_iterator to nested_iterator. Moved nested_iterator to its own header. Documented exception safety for unordered_map.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@402 add3d541-d23a-0410-8454-c3306b179d20

--- src/cpp/siblings/unordered_map.hpp
+++ src/cpp/siblings/unordered_map.hpp
@@ -1,6 +1,9 @@
+// Copyright 2007 Mikael Lind
+
 #ifndef SIBLINGS_UNORDERED_MAP_HPP
 #define SIBLINGS_UNORDERED_MAP_HPP
 
+#include "nested_iterator.hpp"
 #include <cassert>
 #include <cstddef>
 #include <functional>
@@ -9,119 +12,9 @@
 #include <vector>
 #include <boost/foreach.hpp>
 #include <boost/functional/hash.hpp>
-#include <boost/iterator/iterator_facade.hpp>
 #include <boost/utility.hpp>
 
 namespace siblings {
-    template <typename T, typename BucketIterator, typename LocalIterator>
-    class unordered_map_iterator
-        : public boost::iterator_facade<unordered_map_iterator<T,
-                                                               BucketIterator,
-                                                               LocalIterator>,
-                                        T, boost::forward_traversal_tag>
-    {
-    public:
-        typedef T value_type;
-        typedef BucketIterator bucket_iterator;
-        typedef LocalIterator local_iterator;
-
-        unordered_map_iterator()
-            : current_bucket_(), last_bucket_(),
-              current_value_(), last_value_()
-        { }
-
-        unordered_map_iterator(bucket_iterator current_bucket,
-                               bucket_iterator last_bucket)
-            : current_bucket_(current_bucket), last_bucket_(last_bucket),
-              current_value_(), last_value_()
-        {
-            skip_empty_buckets();
-        }
-
-        /// @pre current_bucket != last_bucket
-        /// @pre current_value != current_bucket->end()
-        /// @post i.current_bucket() == current_bucket
-        /// @post i.last_bucket() == last_bucket
-        /// @post i.current_value() == current_value
-        /// @post i.last_value() == current_bucket->end()
-        unordered_map_iterator(bucket_iterator current_bucket,
-                               bucket_iterator last_bucket,
-                               local_iterator current_value)
-            : current_bucket_(current_bucket), last_bucket_(last_bucket),
-              current_value_(current_value), last_value_(current_bucket->end())
-        {
-            assert(current_bucket != last_bucket);
-            assert(current_value != current_bucket->end());
-        }
-
-        template <typename ConstIterator>
-        unordered_map_iterator(const ConstIterator& other)
-            : current_bucket_(other.current_bucket()),
-              last_bucket_(other.last_bucket()),
-              current_value_(other.current_value()),
-              last_value_(other.last_value())
-        { }
-
-        bucket_iterator current_bucket() const { return current_bucket_; }
-        bucket_iterator last_bucket() const { return last_bucket_; }
-        local_iterator current_value() const { return current_value_; }
-        local_iterator last_value() const { return last_value_; }
-
-    private:
-        friend class boost::iterator_core_access;
-
-        bucket_iterator current_bucket_;
-        bucket_iterator last_bucket_;
-        local_iterator current_value_;
-        local_iterator last_value_;
-
-        /// @pre i.current_bucket() != i.last_bucket()
-        /// @pre i.current_value() != i.last_value()
-        void increment()
-        {
-            assert(current_bucket() != last_bucket());
-            assert(current_value() != last_value());
-            ++current_value_;
-            if (current_value_ == last_value_) {
-                ++current_bucket_;
-                skip_empty_buckets();
-            }
-        }
-
-        /// @pre i.last_bucket() == other.last_bucket()
-        bool equal(const unordered_map_iterator& other) const
-        {
-            assert(last_bucket() == other.last_bucket());
-            return current_bucket_ == other.current_bucket_
-                && (current_bucket_ == last_bucket_
-                    || current_value_ == other.current_value_);
-        }
-        
-        /// @pre i.current_bucket() != i.last_bucket()
-        /// @pre i.current_value() != i.last_value()
-        value_type& dereference() const
-        {
-            assert(current_bucket() != last_bucket());
-            assert(current_value() != last_value());
-            return *current_value_;
-        }
-
-        void skip_empty_buckets()
-        {
-            while (current_bucket_ != last_bucket_ && current_bucket_->empty())
-            {
-                ++current_bucket_;
-            }
-            if (current_bucket_ != last_bucket_) {
-                current_value_ = current_bucket_->begin();
-                last_value_ = current_bucket_->end();
-            } else {
-                current_value_ = local_iterator();
-                last_value_ = local_iterator();
-            }
-        }
-    };
-    
     /// @invariant m.load_factor() <= m.max_load_factor()
     template <typename K, typename T, typename H = boost::hash<K>,
               typename P = std::equal_to<K>,
@@ -156,12 +49,12 @@ namespace siblings {
         typedef typename bucket_type::iterator local_iterator;
         typedef typename bucket_type::const_iterator const_local_iterator;
 
-        typedef unordered_map_iterator<value_type, bucket_iterator,
-                                       local_iterator>
+        typedef nested_iterator<value_type, bucket_iterator,
+                                local_iterator>
         iterator;
 
-        typedef unordered_map_iterator<const value_type, const_bucket_iterator,
-                                       const_local_iterator>
+        typedef nested_iterator<const value_type, const_bucket_iterator,
+                                const_local_iterator>
         const_iterator;
 
         // lifecycle //////////////////////////////////////////////////////////
@@ -176,6 +69,7 @@ namespace siblings {
               allocator_(a), size_(0), max_load_factor_(1)
         { }
 
+        /// Exception safety: No-throw guarantee.
         void swap(unordered_map& other)
         {
             buckets_.swap(other.buckets_);
@@ -188,21 +82,25 @@ namespace siblings {
 
         // iteration //////////////////////////////////////////////////////////
         
+        /// Exception safety: No-throw guarantee.
         iterator begin()
         {
             return iterator(buckets_.begin(), buckets_.end());
         }
 
+        /// Exception safety: No-throw guarantee.
         const_iterator begin() const
         {
             return const_iterator(buckets_.begin(), buckets_.end());
         }
 
+        /// Exception safety: No-throw guarantee.
         iterator end()
         {
             return iterator(buckets_.end(), buckets_.end());
         }
 
+        /// Exception safety: No-throw guarantee.
         const_iterator end() const
         {
             return const_iterator(buckets_.end(), buckets_.end());
@@ -210,6 +108,8 @@ namespace siblings {
 
         // local iteration ////////////////////////////////////////////////////
 
+        /// Exception safety: No-throw guarantee.
+        ///
         /// @pre i < m.bucket_count()
         local_iterator begin(size_type i)
         {
@@ -217,6 +117,8 @@ namespace siblings {
             return buckets_[i].begin();
         }
 
+        /// Exception safety: No-throw guarantee.
+        ///
         /// @pre i < m.bucket_count()
         const_local_iterator begin(size_type i) const
         {
@@ -224,6 +126,8 @@ namespace siblings {
             return buckets_[i].begin();
         }
 
+        /// Exception safety: No-throw guarantee.
+        ///
         /// @pre i < m.bucket_count()
         local_iterator end(size_type i)
         {
@@ -231,6 +135,8 @@ namespace siblings {
             return buckets_[i].end();
         }
 
+        /// Exception safety: No-throw guarantee.
+        ///
         /// @pre i < m.bucket_count()
         const_local_iterator end(size_type i) const
         {
@@ -240,6 +146,8 @@ namespace siblings {
 
         // insertion and removal //////////////////////////////////////////////
 
+        /// Exception safety: Basic guarantee.
+        ///
         // @post m.find(v.first) != m.end()
         std::pair<iterator, bool> insert(const value_type& v)
         {
@@ -269,6 +177,7 @@ namespace siblings {
             return result;
         }
 
+        /// Exception safety: Basic guarantee.
         template <typename InputIterator>
         void insert(InputIterator first, InputIterator last)
         {
@@ -277,14 +186,18 @@ namespace siblings {
             }
         }
 
+        /// Exception safety: No-throw guarantee.
         void erase(iterator i)
         {
-            assert(i.current_bucket() != i.last_bucket());
-            assert(i.current_value() != i.last_value());
-            i.current_bucket()->erase(i.current_value());
+            assert(i.current_outer() != i.last_outer());
+            assert(i.current_inner() != i.last_inner());
+            i.current_outer()->erase(i.current_inner());
             --size_;
         }
 
+        /// Exception safety: No-throw guarantee if this is offered by the hash
+        /// and equal operations; strong guarantee otherwise.
+        ///
         /// @post m.find(k) == m.end()
         size_type erase(const key_type& k)
         {
@@ -301,6 +214,7 @@ namespace siblings {
             return result;
         }
 
+        /// Exception safety: No-throw guarantee.
         void clear()
         {
             BOOST_FOREACH(bucket_type& b, buckets_) {
@@ -309,6 +223,8 @@ namespace siblings {
             size_ = 0;
         }
 
+        /// Exception safety: Basic guarantee. This operation is implemented as
+        /// an insert operation, which only offers the basic guarantee.
         mapped_type& operator[](const key_type& k)
         {
             return insert(value_type(k, mapped_type())).first->second;
@@ -316,6 +232,9 @@ namespace siblings {
 
         // searching //////////////////////////////////////////////////////////
 
+        /// Exception safety: No-throw guarantee if this is offered by the hash
+        /// and equal operations; strong guarantee otherwise.
+        ///
         /// @post result == m.end() || result->first == k
         iterator find(const key_type& k)
         {
@@ -325,6 +244,9 @@ namespace siblings {
             return (v == b->end()) ? end() : iterator(b, buckets_.end(), v);
         }
 
+        /// Exception safety: No-throw guarantee if this is offered by the hash
+        /// and equal operations; strong guarantee otherwise.
+        ///
         /// @post result == m.end() || result->first == k
         const_iterator find(const key_type& k) const
         {
@@ -337,40 +259,61 @@ namespace siblings {
 
         // size ///////////////////////////////////////////////////////////////
 
+        /// Exception safety: No-throw guarantee.
+        ///
         /// @return The number of key-value pairs in the map.
         size_type size() const { return size_; }
 
+        /// Exception safety: No-throw guarantee.
+        ///
         /// @return True if the map contains no key-value pairs; false
         ///         otherwise.
         bool empty() const { return size() == 0; }
 
         // partitioning ///////////////////////////////////////////////////////
 
+        /// Exception safety: No-throw guarantee.
+        ///
         /// @return The number of buckets in the map.
         size_type bucket_count() const { return buckets_.size(); }
 
+        /// Exception safety: No-throw guarantee if this is offered by the hash
+        /// operation; strong guarantee otherwise.
+        ///
         /// @return The bucket index for the specified key.
-        /// @post result < m.size()
+        /// @post result < m.bucket_count()
         size_type bucket(const key_type& k) const
         {
             return hash_(k) % bucket_count();
         }
 
+        /// Exception safety: No-throw guarantee.
+        ///
         size_type bucket_size(size_type i) const
         {
             assert(i <= bucket_count());
             return buckets_[i].size();
         }
 
+        /// Exception safety: No-throw guarantee.
+        ///
         // @post result >= 0 && result <= m.max_load_factor()
         float load_factor() const
         {
             return load_factor(bucket_count());
         }
 
+        /// Exception safety: No-throw guarantee.
+        ///
         // @post result >= 0
         float max_load_factor() const { return max_load_factor_; }
 
+        /// Exception safety: Strong guarantee. Exceptions can only be thrown
+        /// when constructing the new bucket vector. The map instance has not
+        /// been modified at this point. Once the new bucket vector has been
+        /// constructed, the rest of the rehashing is carried out using list
+        /// splicing and vector swapping, which are no-throw operations.
+        ///
         /// @post m.bucket_count() >= n
         void rehash(size_type n)
         {
@@ -416,6 +359,7 @@ namespace siblings {
 
         float load_factor(size_type n) const
         {
+            assert(n >= 1);
             return float(size()) / float(n);
         }
     };
