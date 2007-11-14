f3d24dbf88a92dada0bc8ef29283214416a02511
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-11-14 13:30:48 +0000

    Improved Doxygen comments for unordered_multimap.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@434 add3d541-d23a-0410-8454-c3306b179d20

--- src/cpp/siblings/unordered_map.hpp
+++ src/cpp/siblings/unordered_map.hpp
@@ -342,29 +342,63 @@ namespace siblings {
         impl_type;
 
     public:
-        // types //////////////////////////////////////////////////////////////
+        /// @name Types
+        /// @{
 
+        /// @copydoc detail::unordered_impl::key_type
         typedef typename impl_type::key_type key_type;
+
+        /// @copydoc detail::unordered_impl::value_type
         typedef typename impl_type::value_type value_type;
+
+        /// Mapped type.
         typedef T mapped_type;
+
+        /// @copydoc detail::unordered_impl::size
         typedef typename impl_type::hasher hasher;
+
+        /// @copydoc detail::unordered_impl::key_equal
         typedef typename impl_type::key_equal key_equal;
+
+        /// @copydoc detail::unordered_impl::allocator_type
         typedef typename impl_type::allocator_type allocator_type;
 
+        /// @copydoc detail::unordered_impl::pointer
         typedef typename impl_type::pointer pointer;
+
+        /// @copydoc detail::unordered_impl::const_pointer
         typedef typename impl_type::const_pointer const_pointer;
+
+        /// @copydoc detail::unordered_impl::reference
         typedef typename impl_type::reference reference;
+
+        /// @copydoc detail::unordered_impl::const_reference
         typedef typename impl_type::const_reference const_reference;
+
+        /// @copydoc detail::unordered_impl::size_type
         typedef typename impl_type::size_type size_type;
+
+        /// @copydoc detail::unordered_impl::difference_type
         typedef typename impl_type::difference_type difference_type;
 
+        /// @copydoc detail::unordered_impl::iterator
         typedef typename impl_type::iterator iterator;
+
+        /// @copydoc detail::unordered_impl::const_iterator
         typedef typename impl_type::const_iterator const_iterator;
+
+        /// @copydoc detail::unordered_impl::local_iterator
         typedef typename impl_type::local_iterator local_iterator;
+
+        /// @copydoc detail::unordered_impl::const_local_iterator
         typedef typename impl_type::const_local_iterator const_local_iterator;
 
-        // construct/destroy/copy /////////////////////////////////////////////
+        /// @}
 
+        /// @name Construct/Destroy/Copy
+        /// @{
+
+        /// Default constructor.
         explicit unordered_multimap(size_type n
                                     = impl_type::default_bucket_count,
                                     const hasher& hf = hasher(),
@@ -373,6 +407,7 @@ namespace siblings {
             : impl_(n, typename impl_type::get_key(), hf, eql, a)
         { }
 
+        /// Range constructor.
         template <class InputIterator>
         unordered_multimap(InputIterator f, InputIterator l,
                            size_type n = impl_type::default_bucket_count,
@@ -384,112 +419,211 @@ namespace siblings {
             impl_.insert(f, l);
         }
 
-        // unordered_multimap(const unordered_multimap&);
-        // ~unordered_multimap();
-        // unordered_multimap& operator=(const unordered_multimap&);
+        /// Copy constructor.
+        unordered_multimap(const unordered_multimap& other)
+            : impl_(other.impl_)
+        { }
 
+        /// Destructor.
+        ~unordered_multimap() { }
+
+        /// Assignment operator.
+        unordered_multimap& operator=(const unordered_multimap& other)
+        {
+            impl_ = other.impl_;
+            return *this;
+        }
+
+        /// @copydoc detail::unordered_impl::get_allocator
         allocator_type get_allocator() const { impl_.get_allocator(); }
 
-        // size and capacity //////////////////////////////////////////////////
+        /// @}
 
+        /// @name Size and Capacity
+        /// @{
+
+        /// @copydoc detail::unordered_impl::empty
         bool empty() const { return impl_.empty(); }
+
+        /// @copydoc detail::unordered_impl::size
         size_type size() const { return impl_.size(); }
+
+        /// @copydoc detail::unordered_impl::max_size
         size_type max_size() const { return impl_.max_size(); }
 
-        // iterators //////////////////////////////////////////////////////////
+        /// @}
 
+        /// @name Iterators
+        /// @{
+
+        /// @copydoc detail::unordered_impl::begin()
         iterator begin() { return impl_.begin(); }
+
+        /// @copydoc detail::unordered_impl::begin()
         const_iterator begin() const { return impl_.begin(); }
+
+        /// @copydoc detail::unordered_impl::end()
         iterator end() { return impl_.end(); }
+
+        /// @copydoc detail::unordered_impl::end()
         const_iterator end() const { return impl_.end(); }
+
+        /// @copydoc detail::unordered_impl::cbegin()
         const_iterator cbegin() const { return impl_.cbegin(); }
+
+        /// @copydoc detail::unordered_impl::cend()
         const_iterator cend() const { return impl_.cend(); }
 
-        // modifiers //////////////////////////////////////////////////////////
+        /// @}
 
+        /// @name Modifiers
+        /// @{
+
+        /// @copydoc detail::unordered_impl::insert(const value_type&)
         std::pair<iterator, bool> insert(const value_type& obj)
         {
             return impl_.insert(obj);
         }
 
+        /// @copydoc
+        /// detail::unordered_impl::insert(iterator,const value_type&)
         iterator insert(iterator hint, const value_type& obj)
         {
             return impl_.insert(hint, obj);
         }
 
+        /// @copydoc
+        /// detail::unordered_impl::insert(const_iterator,const value_type&)
         const_iterator insert(const_iterator hint, const value_type& obj)
         {
             return impl_.insert(hint, obj);
         }
 
+        /// @copydoc
+        /// detail::unordered_impl::insert(InputIterator,InputIterator)
         template <class InputIterator>
         void insert(InputIterator first, InputIterator last)
         {
             return impl_.insert(first, last);
         }
 
+        /// @copydoc detail::unordered_impl::erase(iterator)
         iterator erase(iterator i) { return impl_.erase(i); }
+
+        /// @copydoc detail::unordered_impl::erase(const_iterator)
         const_iterator erase(const_iterator i) { return impl_.erase(i); }
+
+        /// @copydoc detail::unordered_impl::erase(const key_type&)
         size_type erase(const key_type& k) { return impl_.erase(k); }
 
+        /// @copydoc detail::unordered_impl::erase(iterator,iterator)
         iterator erase(iterator first, iterator last)
         {
             return impl_.erase(first, last);
         }
 
+        /// @copydoc
+        /// detail::unordered_impl::erase(const_iterator,const_iterator)
         const_iterator erase(const_iterator first, const_iterator last)
         {
             return impl_.erase(first, last);
         }
 
+        /// @copydoc detail::unordered_impl::clear
         void clear() { impl_.clear(); }
+
+        /// @copydoc detail::unordered_impl::swap
         void swap(unordered_multimap& other) { impl_.swap(other.impl_); }
 
-        // observers //////////////////////////////////////////////////////////
+        /// @}
 
+        /// @name Observers
+        /// @{
+
+        /// @copydoc detail::unordered_impl::hash_function
         hasher hash_function() const { return impl_.hash_function(); }
+
+        /// @copydoc detail::unordered_impl::key_eq
         key_equal key_eq() const { return impl_.key_eq(); }
 
-        // lookup /////////////////////////////////////////////////////////////
+        /// @}
 
+        /// @name Lookup
+        /// @{
+
+        /// @copydoc detail::unordered_impl::find(const key_type&)
         iterator find(const key_type& k) { return impl_.find(k); }
+
+        /// @copydoc detail::unordered_impl::find(const key_type&)
         const_iterator find(const key_type& k) const { return impl_.find(k); }
+
+        /// @copydoc detail::unordered_impl::count
         size_type count(const key_type& k) const { return impl_.count(k); }
 
+        /// @copydoc detail::unordered_impl::equal_range(const key_type&)
         std::pair<iterator, iterator>
         equal_range(const key_type& k) { return impl_.equal_range(k); }
 
+        /// @copydoc detail::unordered_impl::equal_range(const key_type&)
         std::pair<const_iterator, const_iterator>
         equal_range(const key_type& k) const { return impl_.equal_range(k); }
 
-        // bucket interface ///////////////////////////////////////////////////
+        /// @}
 
+        /// @name Bucket Interface
+        /// @{
+
+        /// @copydoc detail::unordered_impl::bucket_count
         size_type bucket_count() const { return impl_.bucket_count(); }
+
+        /// @copydoc detail::unordered_impl::max_bucket_count
         size_type max_bucket_count() const { return impl_.max_bucket_count(); }
 
+        /// @copydoc detail::unordered_impl::bucket_size
         size_type
         bucket_size(size_type i) const { return impl_.bucket_size(i); }
 
+        /// @copydoc detail::unordered_impl::bucket
         size_type bucket(const key_type& k) const { return impl_.bucket(k); }
+
+        /// @copydoc detail::unordered_impl::begin(size_type)
         local_iterator begin(size_type i) { return impl_.begin(i); }
 
+        /// @copydoc detail::unordered_impl::begin(size_type)
         const_local_iterator
         begin(size_type i) const { return impl_.begin(i); }
 
+        /// @copydoc detail::unordered_impl::end(size_type)
         local_iterator end(size_type i) { return impl_.end(i); }
+
+        /// @copydoc detail::unordered_impl::end(size_type)
         const_local_iterator end(size_type i) const { return impl_.end(i); }
 
-        // hash policy ////////////////////////////////////////////////////////
+        /// @}
 
+        /// @name Hash Policy
+        /// @{
+
+        /// @copydoc detail::unordered_impl::load_factor
         float load_factor() const { return impl_.load_factor(); }
+
+        /// @copydoc detail::unordered_impl::max_load_factor()
         float max_load_factor() const { return impl_.max_load_factor(); }
+
+        /// @copydoc detail::unordered_impl::max_load_factor(float)
         void max_load_factor(float z) { impl_.max_load_factor(z); }
+
+        /// @copydoc detail::unordered_impl::rehash
         void rehash(size_type n) { impl_.rehash(n); }
 
+        /// @}
+
     private:
+        /// Implementation.
         impl_type impl_;
     };
 
+    /// Swaps two unordered maps.
     template <class Key, class T, class Hash, class Pred, class Alloc>
     void swap(unordered_map<Key, T, Hash, Pred, Alloc>& x,
               unordered_map<Key, T, Hash, Pred, Alloc>& y)
@@ -497,6 +631,7 @@ namespace siblings {
         x.swap(y);
     }
 
+    /// Swaps two unordered multimaps.
     template <class Key, class T, class Hash, class Pred, class Alloc>
     void swap(unordered_multimap<Key, T, Hash, Pred, Alloc>& x,
               unordered_multimap<Key, T, Hash, Pred, Alloc>& y)
