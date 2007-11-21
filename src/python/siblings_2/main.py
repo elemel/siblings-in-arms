b51c01c25743838e2f1b9bb265c5efa7bbad6164
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-11-21 09:09:44 +0000

    Implemented time steps. Partial implementation of pathfinding queue and path following. Continued grid implementation.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@436 add3d541-d23a-0410-8454-c3306b179d20

--- src/python/siblings_2/main.py
+++ src/python/siblings_2/main.py
@@ -1,27 +1,56 @@
+import time
+
 class GridBucket:
     def __init__(self):
         self.things = []
-        self.blocked = False
+
+    def add_thing(self, thing):
+        self.things.append(thing)
+
+    def remove_thing(self, thing):
+        self.things.erase(thing)
 
 class Grid:
     def __init__(self):
         self.buckets = []
         for x in range(100):
-            self.buckets[x] = []
+            self.buckets.append([])
             for y in range(100):
                 self.buckets[x].append(GridBucket())
+        self.things = {}
+
+    def add_thing(self, thing):
+        x, y = thing.position
+        bucket = self.buckets[int(x)][int(y)]
+        bucket.add_thing(thing)
+        self.things[thing.id] = bucket
+
+    def remove_thing(self, thing):
+        bucket = self.pop[thing.id]
+        bucket.remove_thing(thing)
+
+    def update_thing(self, thing):
+        self.remove_thing(thing)
+        self.add_thing(thing)
 
 class PathFinder:
-    def __call__(self, grid, from_pos, to_pos)
+    def __call__(self, grid, from_pos, to_pos):
+        pass
 
 class Thing:
     _next_id = 1
 
     def __init__(self):
-        self.id = _next_id++
+        self.id = Thing._next_id
+        print "Creating thing #%d." % self.id
+        Thing._next_id += 1
+        self.position = (0, 0)
     
     def update(self, dt, game_engine):
         pass
+
+    def set_position(self, position):
+        self.position = position
 
 class Shot(Thing):
     pass
@@ -29,6 +58,7 @@ class Shot(Thing):
 IDLE = 1
 BEFORE_PATH_REQUEST = 2
 AFTER_PATH_REQUEST = 3
+FOLLOWING_PATH = 4
 
 class Unit(Thing):
     def __init__(self):
@@ -38,18 +68,34 @@ class Unit(Thing):
     
     def update(self, dt, game_engine):
         if self.state == BEFORE_PATH_REQUEST:
-            game_engine.request_path(self, self.waypoints[0])
-            self.state = AFTER_PATH_REQUEST
+            self._request_path(dt, game_engine)
+        elif self.state == FOLLOWING_PATH:
+            self._follow_path(dt, game_engine)
 
     def add_waypoint(self, waypoint):
         self.waypoints.append(waypoint)
         self.state = BEFORE_PATH_REQUEST
+
+    def set_path(self, path):
+        self.path = path
+        self.state = FOLLOWING_PATH
+
+    def _request_path(self, dt, game_engine):
+        game_engine.request_path(self, self.waypoints[0])
+        self.state = AFTER_PATH_REQUEST
+
+    def _follow_path(self, dt, game_engine):
+        if len(self.path) == 0:
+            self.state = IDLE
+        else:
+            pass
 
 class Structure(Thing):
     pass
 
 class GameEngine:
     def __init__(self):
+        self.grid = Grid()
         self.things = []
         self.path_queue = []
         
@@ -57,13 +103,38 @@ class GameEngine:
         for t in self.things:
             t.update(dt, self)
 
-    def request_path(self, unit, destination):
-        path_queue.append((unit, destination))
+    def request_path(self, unit, waypoint):
+        self.path_queue.append((unit, waypoint))
+
+    def update_position(self, thing):
+        self.grid.update_thing(thing)
+
+    def add_thing(self, thing):
+        self.things.append(thing)
+        self.grid.add_thing(thing)
+
+    def remove_thing(self, thing):
+        self.things.erase(thing)
+        self.grid.remove_thing(thing)
+
+MIN_TIME_STEP = 0.01
+MAX_TIME_STEP = 0.1
 
 def main():
     e = GameEngine()
+    t = Thing()
+    t.set_position((17, 23))
+    e.add_thing(t)
+    old_time = time.time()
     while True:
-        e.update()
+        new_time = time.time()
+        dt = new_time - old_time
+        if dt >= MIN_TIME_STEP:
+            if dt > MAX_TIME_STEP:
+                print "Skipping frames."
+                dt = MAX_TIME_STEP
+            old_time = new_time
+            e.update(dt)
 
 if __name__ == "__main__":
     main()
