379736c5e16ab16a84b8d6b21e14b723fe33323d
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-11-21 11:50:54 +0000

    Replaced the Python grid with SparseGrid2.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@440 add3d541-d23a-0410-8454-c3306b179d20

--- src/python/siblings_2/main.py
+++ src/python/siblings_2/main.py
@@ -3,39 +3,6 @@ from siblings.Circle2 import Circle2
 from siblings.SparseGrid2 import SparseGrid2
 from siblings.Vector2 import Vector2
 
-class Grid:
-    def __init__(self):
-        self.buckets = []
-        for x in range(100):
-            self.buckets.append([])
-            for y in range(100):
-                self.buckets[x].append(Grid._Bucket())
-        self.things = {}
-
-    def add_thing(self, thing):
-        x, y = thing.position
-        bucket = self.buckets[int(x)][int(y)]
-        bucket.add_thing(thing)
-        self.things[thing.id] = bucket
-
-    def remove_thing(self, thing):
-        bucket = self.pop[thing.id]
-        bucket.remove_thing(thing)
-
-    def update_thing(self, thing):
-        self.remove_thing(thing)
-        self.add_thing(thing)
-
-    class _Bucket:
-        def __init__(self):
-            self.things = []
-
-        def add_thing(self, thing):
-            self.things.append(thing)
-
-        def remove_thing(self, thing):
-            self.things.erase(thing)
-
 class PathFinder:
     def __call__(self, grid, from_pos, to_pos):
         pass
@@ -47,7 +14,7 @@ class Thing:
         self.id = Thing._next_id
         print "Creating thing #%d." % self.id
         Thing._next_id += 1
-        self.position = (0, 0)
+        self.position = Vector2(0, 0)
     
     def update(self, dt, game_engine):
         pass
@@ -98,7 +65,7 @@ class Structure(Thing):
 
 class GameEngine:
     def __init__(self):
-        self.grid = Grid()
+        self.grid = SparseGrid2()
         self.things = []
         self.path_queue = []
         
@@ -110,15 +77,15 @@ class GameEngine:
         self.path_queue.append((unit, waypoint))
 
     def update_position(self, thing):
-        self.grid.update_thing(thing)
+        self.grid.insert(thing.id, Circle2(thing.position, 0.5))
 
     def add_thing(self, thing):
         self.things.append(thing)
-        self.grid.add_thing(thing)
+        self.update_position(thing)
 
     def remove_thing(self, thing):
         self.things.erase(thing)
-        self.grid.remove_thing(thing)
+        self.grid.erase(thing.id)
 
 MIN_TIME_STEP = 0.01
 MAX_TIME_STEP = 0.1
@@ -126,8 +93,9 @@ MAX_TIME_STEP = 0.1
 def main():
     e = GameEngine()
     t = Thing()
-    t.set_position((17, 23))
+    t.set_position(Vector2(17, 23))
     e.add_thing(t)
+    e.update_position(t)
     old_time = time.time()
     while True:
         new_time = time.time()
