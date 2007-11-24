ec337c46c83cd91e0aaa34e3c8de97f7f8cb67bd
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-11-24 16:13:09 +0000

    Moved the A* test to its own file.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@443 add3d541-d23a-0410-8454-c3306b179d20

--- src/python/siblings_2/a_star.py
+++ src/python/siblings_2/a_star.py
@@ -1,41 +1,39 @@
-import math, sys
+import math
 from heapq import heappush, heappop, heapify
 
 SQRT_2 = math.sqrt(2)
+OPEN = 1
+CLOSED = 2
 
-class Node:
-    def __init__(self, id_, p, h, g = 0.0, parent = None, closed = False):
-        self.id_ = id_
+class Node(object):
+    def __init__(self, p, h, g = 0.0, parent = None, state = OPEN):
         self.p = p
         self.h = h
         self.g = g
         self.parent = parent
-        self.closed = closed
+        self.state = OPEN
 
     def f(self):
         return self.g + self.h
 
     def __cmp__(self, other):
-        # break ties with ID, preferring newly created nodes
-        return cmp(self.f(), other.f()) or -cmp(self.id_, other.id_)
+        # break ties with object ID
+        return cmp(self.f(), other.f()) or cmp(id(self), id(other))
+
+    def __iter__(self):
+        node = self
+        while node is not None:
+            yield node
+            node = node.parent
 
     def __str__(self):
-        return "[%s]" % self._str()
-    
-    def _str(self):
-        if self.parent is None:
-            return str(self.p)
-        else:
-            return "%s, %s" % (self.p, self.parent._str())
-
-    def __repr__(self):
-        return "Node#%d" % self.id_
+        return str([n.p for n in self])
 
 def grid_neighbors(p):
     x, y = p
-    return [(x - 1, y + 1), (x + 0, y + 1), (x + 1, y + 1),
-            (x - 1, y + 0),                 (x + 1, y + 0),
-            (x - 1, y - 1), (x + 0, y - 1), (x + 1, y - 1)]
+    yield x - 1, y + 1; yield x + 0, y + 1; yield x + 1, y + 1
+    yield x - 1, y + 0;                     yield x + 1, y + 0
+    yield x - 1, y - 1; yield x + 0, y - 1; yield x + 1, y - 1
 
 def manhattan_distance(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
@@ -45,85 +43,34 @@ def diagonal_distance(a, b):
     dy = abs(a[1] - b[1])
     return min(dx, dy) * SQRT_2 + abs(dx - dy)
 
-def true_predicate(a):
-    return True
-
-def a_star(start, goal, neighbors = grid_neighbors, predicate = true_predicate,
-           cost = diagonal_distance, heuristic = diagonal_distance):
-    ids = iter(xrange(sys.maxint))
-    nodes = {}
-    best_h = heuristic(start, goal)
-    best = Node(ids.next(), start, best_h)
-    nodes[start] = best
-    q = []
-    heappush(q, best)
+def a_star(start, goal, neighbors, cost, heuristic):
+    best_h = heuristic(start)
+    best = Node(start, best_h)
+    nodes = {start: best}
+    q = [best]
     while q:
         current = heappop(q)
-        current.closed = True
-        for n in filter(predicate, neighbors(current.p)):
-            neighbor = nodes.get(n, None)
+        current.state = CLOSED
+        for n in neighbors(current.p):
             neighbor_g = current.g + cost(current.p, n)
+            neighbor = nodes.get(n, None)
             if neighbor is None:
-                neighbor = Node(ids.next(), n, heuristic(n, goal), neighbor_g,
-                                current)
+                neighbor = Node(n, heuristic(n), neighbor_g, current)
                 if neighbor.p == goal:
                     return neighbor, nodes
                 nodes[n] = neighbor
                 heappush(q, neighbor)
+                if neighbor.h < best_h:
+                    best_h = neighbor.h
+                    best = neighbor
             elif neighbor_g < neighbor.g:
                 neighbor.parent = current
                 neighbor.g = neighbor_g
-                if neighbor.closed:
-                    neighbor.closed = False
+                if neighbor.state == CLOSED:
+                    # reopen neighbor node
+                    neighbor.state = OPEN
                     heappush(q, neighbor)
                 else:
+                    # reestablish heap invariant
                     heapify(q)
     return best, nodes
-
-def create_grid(width, height, symbol):
-    grid = []
-    for x in range(width):
-        grid.append([symbol] * height)
-    return grid    
-
-def paint_block(grid, min_, max_, symbol):
-    for x in xrange(min_[0], max_[0] + 1):
-        for y in xrange(min_[1], max_[1] + 1):
-            grid[x][y] = symbol
-
-def paint_nodes(grid, nodes, symbol):
-    for id_, n in nodes.iteritems():
-        x, y = n.p
-        grid[x][y] = symbol
-
-def paint_path(grid, path, symbol):
-    while path is not None:
-        x, y = path.p
-        grid[x][y] = symbol
-        path = path.parent
-
-def print_grid(g):
-    height = len(g[0])
-    for y in xrange(height - 1, -1, -1):
-        print "".join(g[x][y] for x in xrange(len(g)))
-
-def test():
-    width, height = 75, 20
-    grid = create_grid(width, height, " ")
-    paint_block(grid, (17, 2), (19, 15), "#")
-    paint_block(grid, (35, 5), (38, 18), "#")
-
-    def predicate(p):
-        x, y = p
-        return (x >= 0 and x < width and y >= 0 and y < height
-                and grid[x][y] != "#")
-
-    path, nodes = a_star((4, 13), (44, 7), predicate = predicate)
-    print "Examined nodes: %d" % len(nodes)
-    print "Path cost: %f" % path.f()
-    paint_nodes(grid, nodes, ".")
-    paint_path(grid, path, "@")
-    print_grid(grid)
-
-if __name__ == "__main__":
-    test()
---
+++ src/python/siblings_2/a_star_test.py
@@ -0,0 +1,69 @@
+from a_star import a_star, grid_neighbors, diagonal_distance
+
+def create_grid(width, height, symbol):
+    grid = []
+    for x in range(width):
+        grid.append([symbol] * height)
+    return grid    
+
+def paint_block(grid, min_, max_, symbol):
+    for x in xrange(min_[0], max_[0] + 1):
+        for y in xrange(min_[1], max_[1] + 1):
+            grid[x][y] = symbol
+
+def paint_nodes(grid, nodes, symbol):
+    for n in nodes.itervalues():
+        x, y = n.p
+        grid[x][y] = symbol
+
+def paint_path(grid, path, symbol):
+    while path is not None:
+        x, y = path.p
+        grid[x][y] = symbol
+        path = path.parent
+
+def create_blocked_grid():
+    grid = create_grid(75, 20, " ")
+    paint_block(grid, (6, 2), (16, 3), "#")
+    paint_block(grid, (17, 2), (19, 15), "#")
+    paint_block(grid, (35, 5), (38, 18), "#")
+    paint_block(grid, (39, 17), (63, 18), "#")
+    return grid
+
+def print_grid(g):
+    height = len(g[0])
+    for y in xrange(height - 1, -1, -1):
+        print "".join(g[x][y] for x in xrange(len(g)))
+
+def main():
+    grid = create_blocked_grid()
+    width, height = len(grid), len(grid[0])
+    start, goal = (5, 9), (47, 14)
+
+    def neighbors(p):
+        def contains(p):
+            x, y = p
+            return x >= 0 and x < width and y >= 0 and y < height
+
+        def passable(p):
+            x, y = p
+            return grid[x][y] != "#"
+
+        return (n for n in grid_neighbors(p) if contains(n) and passable(n))
+
+    def heuristic(p):
+        return diagonal_distance(p, goal)
+
+    path, nodes = a_star(start, goal, neighbors, diagonal_distance,
+                         heuristic)
+
+    print "Path:", path
+    print "Path cost:", path.f()
+    print "Examined nodes:", len(nodes)
+    paint_nodes(grid, nodes, ".")
+    paint_path(grid, path, "@")
+    print_grid(grid)
+    print "Legend: '@' = path, '#' = obstacle, '.' = examined node"
+
+if __name__ == "__main__":
+    main()
