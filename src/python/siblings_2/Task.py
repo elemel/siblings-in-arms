c288f0b4d22c44b41a5b157ea0a98a7d43993db1
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-11-29 10:37:12 +0000

    Added doc strings to Task.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@479 add3d541-d23a-0410-8454-c3306b179d20

--- src/python/siblings_2/Task.py
+++ src/python/siblings_2/Task.py
@@ -1,3 +1,5 @@
+# Copyright 2007 Mikael Lind.
+
 from a_star_search import diagonal_distance
 
 def interpolate_pos(old_p, new_p, progress):
@@ -12,20 +14,37 @@ class TaskError(Exception):
         self.message = message
 
 class Task:
+    """Task for deferred execution by a unit.
+
+    Tasks follow the Command pattern; their execution can be deferred until
+    later. Tasks are enqueued in units and executed in FIFO order. Tasks can
+    also be created and executed by other tasks.
+    
+    Tasks provide cooperative multiprocessing using Python generators. It is
+    generally simpler to implement a complex task as a generator than as a
+    state machine. For tracking purposes, tasks yield their current progress as
+    a float in the range [0.0, 1.0].
+    """
+    
     def __init__(self):
         self._result = None
         self._aborting = False
 
-    def get_result(self): return self._result
-    def is_aborting(self): return self._aborting
+    def _get_result(self): return self._result
+    def _get_aborting(self): return self._aborting
 
-    result = property(get_result)
-    aborting = property(is_aborting)
+    result = property(_get_result, doc = "The result of the task.")
+    aborting = property(_get_aborting, doc = "Is the task aborting?")
 
     def run(self, facade):
+        """Create generator for cooperative multiprocessing.
+
+        To be implemented by subclasses.
+        """
         raise NotImplementedException()
 
     def abort(self):
+        """Request the task to abort."""
         self._aborting = True
 
 class MoveTask(Task):
@@ -35,7 +54,7 @@ class MoveTask(Task):
 
     def run(self, facade):
         old_pos = facade.unit.pos
-        required = (diagonal_distance(old_pos, self.pos) / facade.unit.speed)
+        required = diagonal_distance(old_pos, self.pos) / facade.unit.speed
         elapsed = 0.0
         while True:
             elapsed += facade.dt
