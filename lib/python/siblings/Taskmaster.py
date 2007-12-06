a5cb263df287fea0b582cc4ece475552c43685be
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-12-06 12:10:43 +0000

    Renamed some variables in Taskmaster.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@550 add3d541-d23a-0410-8454-c3306b179d20

--- lib/python/siblings/Taskmaster.py
+++ lib/python/siblings/Taskmaster.py
@@ -4,33 +4,33 @@ from TaskQueue import TaskQueue
 
 class Taskmaster:
     def __init__(self, task_facade):
-        self._tasks = {}
-        self._old_tasks = []
+        self._queues = {}
+        self._empty_queues = []
         self._new_tasks = []
         self._task_facade = task_facade
         
     def update(self, dt):
         self._task_facade.dt = dt
-        for key, tasks in self._tasks.iteritems():
-            tasks.update(self._task_facade)
-            if not tasks:
-                self._old_tasks.append(key)
-        if self._old_tasks:
-            for key in self._old_tasks:
-                del self._tasks[key]
+        for key, queue in self._queues.iteritems():
+            queue.update(self._task_facade)
+            if not queue:
+                self._empty_queues.append(key)
+        if self._empty_queues:
+            for key in self._empty_queues:
+                del self._queues[key]
                 print "Unit #%d fell asleep." % key
-            del self._old_tasks[:]
+            del self._empty_queues[:]
         if self._new_tasks:
             for unit, task in self._new_tasks:
-                if unit.key not in self._tasks:
-                    self._tasks[unit.key] = TaskQueue(unit)
+                if unit.key not in self._queues:
+                    self._queues[unit.key] = TaskQueue(unit)
                     print "Unit #%d woke up." % unit.key
-                self._tasks[unit.key].append(task)
+                self._queues[unit.key].append(task)
             del self._new_tasks[:]
 
     def append_task(self, unit, task):
         self._new_tasks.append((unit, task))
 
     def clear_tasks(self, unit):
-        if unit.key in self._tasks:
-            self._tasks[unit.key].clear()
+        if unit.key in self._queues:
+            self._queues[unit.key].clear()
