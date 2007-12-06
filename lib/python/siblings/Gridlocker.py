a105994476aeb514cae949e9f28f0efd3dd8a200
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-12-06 08:38:46 +0000

    Renamed a couple of member variables.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@544 add3d541-d23a-0410-8454-c3306b179d20

--- lib/python/siblings/Gridlocker.py
+++ lib/python/siblings/Gridlocker.py
@@ -6,8 +6,8 @@ from a_star_search import breadth_first_search
 class Gridlocker:
     def __init__(self):
         self._size = (100, 100)
-        self._locked_cells = {}
-        self._locking_units = {}
+        self._locks = {}
+        self._lockers = {}
 
     def _get_size(self):
         return self._size
@@ -15,33 +15,33 @@ class Gridlocker:
     size = property(_get_size)
         
     def lock_cell(self, unit, pos):
-        old_key = self._locked_cells.get(pos, None)
+        old_key = self._locks.get(pos, None)
         if old_key is None:
-            self._locked_cells[pos] = unit.key
-            if not unit.key in self._locking_units:
-                self._locking_units[unit.key] = set()
-            self._locking_units[unit.key].add(pos)
+            self._locks[pos] = unit.key
+            if not unit.key in self._lockers:
+                self._lockers[unit.key] = set()
+            self._lockers[unit.key].add(pos)
             print "Unit #%d locked cell %s." % (unit.key, pos)
             return True
         else:
             return unit.key == old_key
 
     def unlock_cell(self, unit, pos):
-        if pos in self._locked_cells:
-            del self._locked_cells[pos]
-            self._locking_units[unit.key].remove(pos)
-            if not self._locking_units[unit.key]:
-                del self._locking_units[unit.key]
+        if pos in self._locks:
+            del self._locks[pos]
+            self._lockers[unit.key].remove(pos)
+            if not self._lockers[unit.key]:
+                del self._lockers[unit.key]
             print "Unit #%d unlocked cell %s." % (unit.key, pos)
 
     def unlock_all_cells(self, unit):
-        if unit.key in self._locking_units:
-            for pos in list(self._locking_units[unit.key]):
+        if unit.key in self._lockers:
+            for pos in list(self._lockers[unit.key]):
                 self.unlock_cell(unit, pos)
 
     def find_unlocked_cell(self, start):
         def predicate(pos):
-            return pos not in self._locked_cells
+            return pos not in self._locks
     
         def contains(pos):
             width, height = self._size
@@ -57,4 +57,4 @@ class Gridlocker:
         return path.p
 
     def get_locking_unit(self, pos):
-        return self._locked_cells.get(pos, 0)
+        return self._locks.get(pos, 0)
