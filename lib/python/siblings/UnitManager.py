3e3676adfdd099bfc29de9282dcfa57a509c3860
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-12-10 20:53:38 +0000

    Removed Event.py. Tweaked units. Added a copyright statement.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@582 add3d541-d23a-0410-8454-c3306b179d20

--- lib/python/siblings/UnitManager.py
+++ lib/python/siblings/UnitManager.py
@@ -3,41 +3,43 @@
 from Unit import Unit, UnitSpec
 
 knight_spec = UnitSpec("knight")
-knight_spec.speed = 7.0
+knight_spec.speed = 6.0
 knight_spec.damage = 0.3
 
 monk_spec = UnitSpec("monk")
-monk_spec.speed = 5.0
+monk_spec.speed = 4.0
 monk_spec.damage = 0.1
 
 priest_spec = UnitSpec("priest")
-priest_spec.speed = 5.0
+priest_spec.speed = 4.0
 priest_spec.damage = 0.2
 
 ranger_spec = UnitSpec("ranger")
 ranger_spec.speed = 5.0
-ranger_spec.damage = 0.2
+ranger_spec.damage = 0.3
 
 tavern_spec = UnitSpec("tavern")
 tavern_spec.speed = 0.0
 tavern_spec.size = (3, 3)
 
 thief_spec = UnitSpec("thief")
-thief_spec.speed = 5.0
+thief_spec.speed = 4.0
 thief_spec.damage = 0.2
 
 warrior_spec = UnitSpec("warrior")
-warrior_spec.speed = 5.0
-warrior_spec.damage = 0.3
+warrior_spec.speed = 4.0
+warrior_spec.damage = 0.4
 
 wizard_spec = UnitSpec("wizard")
 wizard_spec.speed = 3.0
 wizard_spec.damage = 0.5
+wizard_spec.preattack_time = 1.0
+wizard_spec.postattack_time = 0.5
 
 class UnitManager:
     def __init__(self):
-        self._specs = {} # name: spec
-        self._units = {} # key: unit
+        self._specs = {}
+        self._units = {}
 
         for spec in (knight_spec, monk_spec, priest_spec, ranger_spec,
                      tavern_spec, thief_spec, warrior_spec, wizard_spec):
--- lib/python/siblings/geometry.py
+++ lib/python/siblings/geometry.py
@@ -1,3 +1,5 @@
+# Copyright 2007 Mikael Lind.
+
 import math
 
 SQRT_2 = math.sqrt(2)
