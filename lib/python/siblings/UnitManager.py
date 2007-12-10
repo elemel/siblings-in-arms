6abee0e2d7678344575e539ca58dc9b61387267c
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2007-12-10 13:05:21 +0000

    Moved UnitSpec management into UnitManager.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@575 add3d541-d23a-0410-8454-c3306b179d20

--- lib/python/siblings/UnitManager.py
+++ lib/python/siblings/UnitManager.py
@@ -34,23 +34,38 @@ wizard_spec = UnitSpec("wizard")
 wizard_spec.speed = 3.0
 wizard_spec.damage = 0.5
 
-unit_specs = {}
-for spec in (knight_spec, monk_spec, priest_spec, ranger_spec, tavern_spec,
-             thief_spec, warrior_spec, wizard_spec):
-    unit_specs[spec.name] = spec
-
 class UnitManager:
     def __init__(self):
-        self._units = {}
-        
+        self._specs = {} # name: spec
+        self._units = {} # key: unit
+
+        for spec in (knight_spec, monk_spec, priest_spec, ranger_spec,
+                     tavern_spec, thief_spec, warrior_spec, wizard_spec):
+            self._specs[spec.name] = spec
+
+    def add_unit_spec(self, spec):
+        self._specs[spec.name] = spec
+
+    def remove_unit_spec(self, spec):
+        del self._specs[spec.name]
+
+    def find_unit_spec(self, name):
+        return self._unit_specs.get(name, None)
+
+    def get_unit_specs(self):
+        return self._specs.itervalues()
+
     def add_unit(self, unit):
         self._units[unit.key] = unit
 
     def remove_unit(self, unit):
         del self._units[unit.key]
 
+    def find_unit(self, key):
+        return self._units.get(key, None)
+
     def get_build_time(self, name):
         return 0.3
 
     def create_unit(self, name, player):
-        return Unit(unit_specs[name], player)
+        return Unit(self._specs[name], player)
