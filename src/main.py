095e968034d4953bfa23e704d769bd83f7742d4d
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2008-07-16 05:20:50 +0000

    Removed the headless option. This is better implemented as a separate UI combined with a demo mode.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@802 add3d541-d23a-0410-8454-c3306b179d20

--- src/main.py
+++ src/main.py
@@ -19,44 +19,20 @@
 # SOFTWARE.
 
 
-import time, sys, random
-from collections import deque
+import time
 from GameEngine import GameEngine
-from Unit import Hero, Tavern, Warrior
-from Task import MoveTask
+import gui
+from Unit import Tavern
 
 
 TIME_STEP = 0.02
 
 
-def random_pos(min_p, max_p):
-    min_x, min_y = min_p
-    max_x, max_y = max_p
-    return random.uniform(min_x, max_x), random.uniform(min_y, max_y)
-
-
 def main():
-    headless = ('-H' in sys.argv or '--headless' in sys.argv)
-    if not headless:
-        try:
-            import gui
-        except ImportError, e:
-            print 'Error when importing GUI:', e
-            print 'Specify the --headless option for a test run in text mode.'
-            sys.exit(1)
-        
     game_engine = GameEngine()
-    game_engine.add_unit(Tavern('cyan'), (5, 14))
-    game_engine.add_unit(Tavern('yellow'), (13, 5))
+    game_engine.add_unit(Tavern('cyan'), (5, 12))
+    game_engine.add_unit(Tavern('yellow'), (12, 5))
 
-    if True or headless:
-        min_p, max_p = (1, 2), (16, 16)
-        colors = 'cyan', 'yellow'
-        hero_types = [Warrior] # Hero.__subclasses__()
-        for i in xrange(50):
-            hero = random.choice(hero_types)(random.choice(colors))
-            game_engine.add_unit(hero, random_pos(min_p, max_p))
-    
     next_time = time.time()
     while True:
         current_time = time.time()
@@ -64,8 +40,7 @@ def main():
             while next_time <= current_time:
                 next_time += TIME_STEP
                 game_engine.update(TIME_STEP)
-            if not headless:
-                gui.update(game_engine)
+            gui.update(game_engine)
         else:
             time.sleep(next_time - current_time)
 
