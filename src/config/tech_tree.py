6cd761cf5c03dc28b1e3d70c3a5060295b38897f
mikkelin <mikkelin@add3d541-d23a-0410-8454-c3306b179d20>
2008-08-02 12:31:28 +0000

    Forgot to add the new config.tech_tree module.
    git-svn-id: https://siblings.googlecode.com/svn/trunk@988 add3d541-d23a-0410-8454-c3306b179d20

---
+++ src/config/tech_tree.py
@@ -0,0 +1,31 @@
+from config.units import *
+from TechTree import TechTree
+
+
+tech_tree = TechTree()
+ 
+# Building dependencies.
+for cls, deps in [(Tavern, []),
+                  (Monastery, [Tavern]),
+                  (Farm, [Monastery]),
+                  (ArcheryRange, [Tavern]),
+                  (Tower, [ArcheryRange]),
+                  (Barracks, [Tavern]),
+                  (Temple, [Tavern]),
+                  (Inn, [Barracks, Temple]),
+                  (GamblingDen, [Inn]),
+                  (Laboratory, [Inn]),
+                  (Stables, [Inn]),
+                  (Hall, [Laboratory, Stables])]:
+    tech_tree.depends(cls, deps)
+
+# Unit dependencies.
+for cls, deps in [(Monk, []),
+                  (Ranger, [ArcheryRange]),
+                  (Warrior, [Barracks]),
+                  (Priest, [Temple]),
+                  (Thief, [GamblingDen]),
+                  (Knight, [Stables]),
+                  (Wizard, [Laboratory])]:
+    tech_tree.depends(cls, deps)
+ 
